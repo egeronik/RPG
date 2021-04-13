70950506d97dd7cce65d478a97d3fa3c79e2ca77
RedStarZ77 <redstarz2002@mail.ru>
2021-04-13 13:31:34 +0300

    Skills

---
+++ OurGame/Assets/Skills/Thunder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunder : Skills
+{
+    public bool Status = false;
+
+    public int Damage;
+
+    private GameObject TeamT, T;
+
+    [SerializeField]
+    private SkillStats _stats;
+    public override SkillStats Stats { get { return _stats; } set { _stats = value; } }
+
+    public override void Activate()
+    {
+        Status = true;
+    }
+
+    void Update()
+    {
+        if (Status)
+        {
+            T = GetComponent<GameMaster>().Target;
+            TeamT = GetComponent<GameMaster>().TeamTarget;
+
+            TeamT.GetComponent<Animator>().SetTrigger("Attack1");
+            T.GetComponent<Vrag>().TakeDamage(Damage);
+
+            Status = false;
+        }
+    }
+}
