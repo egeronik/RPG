48ef3750b37116457843952bbb048e844ab99777
Bronepodrostok <sadchikov.nikita2003@mail.ru>
2021-04-13 11:29:21 +0300

    BattleScene

---
+++ OurGame/Assets/Scripts/MouseTrack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseTrack : MonoBehaviour
+{
+    public GameObject chelic;
+    void Update()
+    {
+            CastRay();
+    }
+    void CastRay() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if (hit) {
+            chelic = hit.collider.gameObject;
+        }
+    }
+
+}
---
+++ OurGame/Assets/Scripts/SkillHUD.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillHUD : MonoBehaviour {
+
+    public SkillStats Stats;
+
+    public Image art;
+
+    public Text description;
+    public Text Name;
+
+    void Start() {
+        //Name.text = Stats.name;
+        //description.text = Stats.description;
+        art.sprite = Stats.artwork;
+    }
+
+}
